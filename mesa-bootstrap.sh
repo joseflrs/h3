c3edf4a2f9286cf8e88c63ef2983bb02d1c9cd3e
Jose <128305409+joseflrs@users.noreply.github.com>
2026-06-20 00:05:15 -0300

    Add files via upload

--- mesa-bootstrap.sh
+++ mesa-bootstrap.sh
@@ -122,7 +122,7 @@ setup_mountpoint $chroot_dir
 chroot $1 apt-get update
 chroot $1 apt-get -y upgrade
 chroot $1 apt-get install -y software-properties-common
-chroot $1 add-apt-repository ppa:mozillateam/ppa
+chroot $1 add-apt-repository -y ppa:mozillateam/ppa
 chroot $1 apt update
 chroot $1 apt-get -y dist-upgrade
 chroot $1 apt-get -y install ubuntu-desktop-minimal gdm3 linux-firmware oem-config-gtk ubiquity-frontend-gtk ubiquity-slideshow-ubuntu yaru-theme-unity yaru-theme-icon yaru-theme-gtk aptdaemon initramfs-tools vim
